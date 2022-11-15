0af23397b21782525bd4fbe1cd12b939e8fb7ab3
Sithum39 <115446245+Sithum39@users.noreply.github.com>
2022-11-15 12:11:51 +0530

    Add files via upload

---
+++ web39/assignment 1.js
@@ -0,0 +1,10 @@
+console.log("I am Sithum");
+
+
+function buy(){
+    
+    console.log("You have bougt");
+    alert("You have bought Successfully...!")
+    
+}
+
---
+++ web39/assignment.js
@@ -0,0 +1,10 @@
+console.log("I am Sithum");
+
+
+function buy(){
+    document.getElementById("razz").innerHTML = "Success";
+    console.log("You have bougt");
+    alert("You have bought Successfully...!")
+    
+}
+
